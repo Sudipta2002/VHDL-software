643cc5ae39a6f21a323c0910c12849dfd9886a42
Sudipta Ghosh <78099311+Sudipta2002@users.noreply.github.com>
2022-07-22 07:22:03 +0530

    Update binary comparator circuit.vhd

--- binary comparator circuit.vhd
+++ binary comparator circuit.vhd
@@ -1,21 +1,20 @@
-Library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
- 
-entity VHDL_Binary_Comparator is
-  port (
-      inp-A,inp-B   : in std_logic_vector(3 downto 0);
-      greater, equal, smaller  : out std_logic
-   );
-end VHDL_Binary_Comparator ;
- 
-architecture bhv of VHDL_Binary_Comparator is
-begin
-greater <= '1' when (inp-A > inp-B)
-else '0';
-equal <= '1' when (inp-A = inp-B)
-else '0';
-smaller <= '1' when (inp-A < inp-B)
-else '0';
-end bhv;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity comp is
+    port(a,b: in std_logic_vector(3 downto 0);
+        g,e,l: out std_logic);
+end comp;
+
+architecture res of comp is
+    begin
+        g<= '1' when (a>b)
+        else '0';
+        l<= '1' when (a<b)
+        else '0';
+        e<= '1' when (a=b)
+        else '0';
+end res;
+
