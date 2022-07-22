1e3c2015251f230c3892985e5953733889064a5d
Sudipta Ghosh <78099311+Sudipta2002@users.noreply.github.com>
2022-07-22 07:20:03 +0530

    Update parity checker.vhd

--- parity checker.vhd
+++ parity checker.vhd
@@ -1,19 +1,25 @@
-library ieee;
-use ieee.std_logic_1164.all;
-entity parity_chk is
-      port( data:in bit_vector(7 downto 0);
-                  p: in bit;
-                  e: out bit);
-end parity_chk;
-architecture parity_arch of parity_chk is
-signal temp : bit_vector(6 downto 0);
-  begin
-    temp(0)<=data(0) xor data(1);
-    temp(1)<=temp(0) xor data(2);
-    temp(2)<=temp(1) xor data(3);
-    temp(3)<=temp(2) xor data(4);
-    temp(4)<=temp(3) xor data(5);
-    temp(5)<=temp(4) xor data(6);
-    temp(6) <= temp(5) xor data(7);
-    e <= p xor temp(6);
-end parity_arch;
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity real is
+    port(a: in bit_vector(7 downto 0);
+         p : in bit;
+         ep: out bit);
+end real;
+
+architecture res of real is
+    signal temp : bit_vector(6 downto 0);
+    begin
+        temp(0)<=a(0) xor a(1);
+        temp(1)<=temp(0) xor a(2);
+        temp(2)<=temp(1) xor a(3);
+        temp(3)<=temp(2) xor a(4);
+        temp(4)<=temp(3) xor a(5);
+        temp(5)<=temp(4) xor a(6);
+        temp(6)<=temp(5) xor a(7);
+        ep<= p xor temp(6);
+end res;
+        
+
+
+
